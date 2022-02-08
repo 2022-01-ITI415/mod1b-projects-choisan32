a37b7deb87b406146555705a987e34bace647bc4
choisan32 <choisan32@gmail.com>
2022-02-08 18:01:22 -0500

    Apple Tree Moves Randomly
    Edited script so the now moves left to right at a reasonable speed. Extended the Left/Right edges. The Tree now randomly changes directions at a reasonable rate.

--- Assets/01-Apple Picker/Scripts/AppleTree.cs
+++ Assets/01-Apple Picker/Scripts/AppleTree.cs
@@ -27,7 +27,27 @@ public class AppleTree : MonoBehaviour
     void Update()
     {
         // Basic Movement
-
+        Vector3 pos = transform.position;
+        pos.x += speed * Time.deltaTime;
+        transform.position = pos;
         // Changing Direction
+        if ( pos.x < -leftAndRightEdge )
+        {
+            speed = Mathf.Abs(speed); // Move right
+        }
+        else if ( pos.x > leftAndRightEdge )
+        {
+            speed = -Mathf.Abs(speed); // Move left
+        }
+        
+    }
+
+    void FixedUpdate ()
+    {
+        // Changing Direction Randomly is now timed better
+        if ( Random.value < chanceToChangeDirections )
+        {
+            speed *= -1; // Change Direction
+        }
     }
 }
