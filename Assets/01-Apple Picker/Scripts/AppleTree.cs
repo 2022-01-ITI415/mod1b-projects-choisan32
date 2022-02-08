ebeadb536da984cb93207569a830223ed7d0b9d6
choisan32 <choisan32@gmail.com>
2022-02-08 17:06:57 -0500

    Set Apple Tree Position
    Changed the Y position of the Apple Tree to 12, so it can be seen in the Game window. Changed the Left and Right values of the tree in the Script to 20.

--- Assets/01-Apple Picker/Scripts/AppleTree.cs
+++ Assets/01-Apple Picker/Scripts/AppleTree.cs
@@ -11,7 +11,7 @@ public class AppleTree : MonoBehaviour
     // Speed at which AppleTree moves
     public float speed = 1f;
     // Distance where AppleTree turns around
-    public float leftAndRightEdge = 10f;
+    public float leftAndRightEdge = 20f;
     // Chance that the AppleTree will change directions
     public float chanceToChangeDirections = 0.1f;
     // Rate at which Apples will be instantiated
