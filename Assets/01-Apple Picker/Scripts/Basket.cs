e91ce96ec63e865354180f58d01fcb46636e2217
choisan32 <choisan32@gmail.com>
2022-02-14 19:25:53 -0500

    Score counts and score saved
    Edited scripts so that the score is updated on screen. High score will now update and save whenever game closes.

--- Assets/01-Apple Picker/Scripts/Basket.cs
+++ Assets/01-Apple Picker/Scripts/Basket.cs
@@ -1,9 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Basket : MonoBehaviour
 {
+    [Header("Set Dynamically")]
+    public Text scoreGT; 
+
+    void Start()
+    {
+        // Find a reference to the ScoireCounter GameObject
+        GameObject scoreGO = GameObject.Find("ScoreCounter");
+
+        // Get the Text Component of that GameObject
+        scoreGT = scoreGO.GetComponent<Text>();
+
+        // Set the starting number of points to 0
+        scoreGT.text = "0";
+    }
 
     // Update is called once per frame
     void Update()
@@ -22,13 +37,29 @@ public class Basket : MonoBehaviour
         pos.x = mousePos3D.x;
         this.transform.position = pos;
     }
+
     void OnCollisionEnter(Collision coll)
     {
         // Find what hits the basket
         GameObject collidedWith = coll.gameObject;
-        if(collidedWith.tag == "Apple")
+        if (collidedWith.tag == "Apple")
         {
             Destroy(collidedWith);
+
+            // Parse the text of the scoreGT into an int
+            int score = int.Parse(scoreGT.text);
+
+            // Add point for catching the apple
+            score += 100;
+
+            // Convert the score back to a string and display it
+            scoreGT.text = score.ToString();
+
+            // Track the high score
+            if(score > HighScore.score)
+            {
+                HighScore.score = score;
+            }
         }
     }
 }
