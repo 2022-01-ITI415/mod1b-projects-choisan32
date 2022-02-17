c73a4ef080a2e73e339b8e5a56cf3933b3feb485
choisan32 <choisan32@gmail.com>
2022-02-17 14:50:11 -0500

    Updated FollowCam script
    Updated script so the camera properly follows the projectile and zooms in and out.

--- Assets/02-Mission Demolition/Scripts/FollowCam.cs
+++ Assets/02-Mission Demolition/Scripts/FollowCam.cs
@@ -6,6 +6,10 @@ public class FollowCam : MonoBehaviour
 {
     static public GameObject POI; // static point of interest
 
+    [Header("Set in Inspector")]
+    public float easing = 0.05f;
+    public Vector2 minXY = Vector2.zero;
+
     [Header("Set Dynamically")]
     public float camZ; // The desired Z pos of the camera
 
@@ -21,9 +25,16 @@ public class FollowCam : MonoBehaviour
 
         // get position of the POI
         Vector3 destination = POI.transform.position;
+        // Limit the X & Y to minimum values
+        destination.x = Mathf.Max( minXY.x, destination.x);
+        destination.y = Mathf.Max( minXY.y, destination.y);
+        // Interlope from the current Camera position toward destination
+        destination = Vector3.Lerp(transform.position, destination, easing);
         // Force destination.z to be camZ to keep the camera far enough away
         destination.z = camZ;
         // Set the camera to be the destination
         transform.position = destination;
+        // Set the orthographicSize of the camera to keep Ground in view
+        Camera.main.orthographicSize = destination.y + 10;
     }
 }
